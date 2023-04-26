02a84bb6a386d3e7ab1ad29401fba27bb7e9cfe9
CoderPear <76975184+CoderPear@users.noreply.github.com>
2023-04-26 13:13:13 -0700

    added lettergrade

--- Ch4.java
+++ Ch4.java
@@ -2,10 +2,11 @@ public class Ch4{
 
 
 	public static void main(String[]args){
-		System.out.println(filter("      Hello   there person   ", ' '));
-		
+		//System.out.println(filter("      Hello   there person   ", ' '));
+		for (int i=55;i<110;i+=10){
+			System.out.println(letterGrade(i));
 		}
-	public static String filter(String x, char y){
+	/*public static String filter(String x, char y){
 		String filtered="";
 		for(int i=0 ; i < x.length();i++){
 			char temp= x.charAt(i);
@@ -15,5 +16,23 @@ public class Ch4{
 			
 			}
 		return filtered;
+	}*/
+}
+	public static char letterGrade( int grade){
+		char letter='0';
+		if(grade>=90){
+				letter = 'A';
+			}else if(grade>=80){
+				letter = 'B';
+			}else if(grade>=70){
+					letter = 'C';
+				}
+			else if(grade>=60){
+					letter = 'D';
+				}
+			else{
+				letter='F';
+				}
+		return letter;
 	}
 }
