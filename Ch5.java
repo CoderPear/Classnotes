a777e46123c224be3d25a70b714f6b270c6338ad
CoderPear <76975184+CoderPear@users.noreply.github.com>
2023-05-03 13:56:43 -0700

    added notes 5-3-23

--- Ch5.java
+++ Ch5.java
@@ -3,8 +3,13 @@ public class Ch5{
 
 
 	public static void main(String[]args){
+		fence();
 		Scanner console=new Scanner(System.in);
+		Random rand=new Random();//psudo-random
+			guessing(console, rand.nextInt(100));
 			
+			for (int i=0;i<10;i++){
+				System.out.print(rand.nextInt(20)+" ");}
 			//System.out.println("Max = "+ max);
 	//}
 	//public static int maxValue(Scanner scan){
@@ -23,8 +28,12 @@ public class Ch5{
 		//return max;
 		//double avg =averageValue2(console);
 			//System.out.println( "average===> "+ avg);
-			double avg= averageValue4(console);
+			/*double avg= averageValue4(console);
 				System.out.println("Ave++>"+avg);
+				avg= (int)((avg*100)/100);
+				System.out.println(avg);
+				System.out.printf("average==>%.2f and the number %8d and string %s",avg,42, "HI");
+				//f for float, d for int and s for string, an num inc digits or spaces.*/
 }
 	public static int averageValue(Scanner scan){
 		System.out.print("Enter number of Numbers ++>");
@@ -76,5 +85,48 @@ public class Ch5{
 			}
 			return total/(n-1);
 	}
+	public static void guessing(Scanner scan, int secret){
+	int guess= scan.nextInt();
+	while(guess != secret){//checks befor look aka ask permission
+			if(guess>secret){
+				System.out.println("too high");
+				}else{
+				System.out.println("too low");}
+			
+			guess=scan.nextInt();
+			}
+			System.out.println("congrats");
+		}
+	public static void guessing2(Scanner scan, int secret){
+	int guess= -1;
+	do{//do loop at least once
+		guess=scan.nextInt();
+			if(guess>secret){
+				System.out.println("too high");
+				}else{
+				System.out.println("too low");}
+			
+			
+			}while(guess != secret);//checks after loop aka asks forgiveness
+			System.out.println("congrats");
+		}
+	public static void fence(){
+		Random rand=new Random();
+		//System.out.print("|");
+		for(int i=0;i<9;i++){
+			if(i%2==0){
+		System.out.print("|");
+	}else{
+		System.out.print("-");}
+		}
+	//System.out.println("|");
+		//for(int i=0;i<5;i++){
+		
+		//System.out.print("|-");
+		//}
+	//System.out.println("|");
 	
+	}
 }
+	
+
