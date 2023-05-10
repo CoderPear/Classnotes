aaf9b7ea28c4f009f7114e1f9f7f1a489100de67
CoderPear <76975184+CoderPear@users.noreply.github.com>
2023-05-10 13:48:48 -0700

    notes 5-10-23

--- files.java
+++ files.java
@@ -2,6 +2,7 @@
 import java.util.*;
 import java.io.*;
 
+
 public class files{
 	
 	
@@ -17,6 +18,7 @@ public class files{
 	}
 
 	public static void main (String[]args) throws FileNotFoundException{
+		
 		/*System.out.println(isRect2(1,2,3,4));
 		System.out.println(isRect2(1,2,1,4));
 		System.out.println(isRect2(1,4,3,4));
@@ -34,15 +36,24 @@ public class files{
 		Scanner console= new Scanner(System.in);
 		//relative from currect dirrectory, how to navigate to intended file.
 		File students = new File("../other/students.txt");
-		Scanner scan = new Scanner(students);
-		while(scan.hasNextLine()){
-		System.out.println("Given: "+scan.next());
-		System.out.println("Family: "+scan.next());
-		System.out.println("Year:\t"+scan.next());
-		System.out.printf("Avg:\t%.2f\n",avg(scan.nextLine()));
-		System.out.println();
+		/*while(!students.canRead()){//asks for permision
+			System.out.println("Enter file name");
+			students=new File(console.nextLine());
+		}*/
+		try{//ask for forgiveness
+			Scanner scan = new Scanner(students);
+				while(scan.hasNextLine()){
+					System.out.println("Given: "+scan.next());
+					System.out.println("Family: "+scan.next());
+					System.out.println("Year:\t"+scan.next());
+					System.out.printf("Avg:\t%.2f\n",avg(scan.nextLine()));
+					System.out.println();
+		}
+		}catch (FileNotFoundException fnfe){
+		System.out.println(fnfe.getMessage());
 	}
-	}
+	System.out.println("gotHere");
+}
 	public static boolean isRect(int a, int b, int c, int d){//correspondind sides not =
 		if(a !=c|| b !=d){
 			return false;
